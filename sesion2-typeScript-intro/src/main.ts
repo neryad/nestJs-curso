a62302ecb20aac59aca0317e05f093aedf4a1b3e
Neryad <dayern.gomez@hotmail.com>
2024-02-23 20:29:00 -0400

    end

--- sesion2-typeScript-intro/src/main.ts
+++ sesion2-typeScript-intro/src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import { charmander } from './bases/04-injectios.ts'
+import { charmander } from './bases/05-decorators.ts'
+
 charmander
 
 //import { name } from './bases/01-types.ts';
