eeecbf4c14a1ec72ab35b5fe7afe2b7fcfbdac78
Neryad <dayern.gomez@hotmail.com>
2024-02-27 11:22:13 -0400

    dtos

--- sesion04-car-dealership/src/cars/cars.controller.ts
+++ sesion04-car-dealership/src/cars/cars.controller.ts
@@ -10,6 +10,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dtos/create-car.dtos';
 
 @Controller('cars')
 export class CarsController {
@@ -27,8 +28,8 @@ export class CarsController {
   }
 
   @Post()
-  createCar(@Body() body: any) {
-    return body;
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return createCarDto;
   }
 
   @Patch(':id')
