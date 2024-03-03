67239e29621962ec32e65cbd95fd777d8222a87e
Neryad <dayern.gomez@hotmail.com>
2024-03-03 16:13:35 -0400

    delete

--- sesion07-pokedex/src/app.module.ts
+++ sesion07-pokedex/src/app.module.ts
@@ -3,6 +3,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       'mongodb+srv://root:Ad123456@devacademy.ruor6i7.mongodb.net/nest-pokemon',
     ),
     PokemonModule,
+    CommonModule,
   ],
 })
 export class AppModule {}
